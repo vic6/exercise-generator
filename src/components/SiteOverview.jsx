15c85bf538be010593c5f4c566bef9fbd9d0e609
vic6 <vicmatthews116@gmail.com>
2018-03-01 19:59:20 -0800

    Add exercise examples to site overview component

--- src/components/SiteOverview.jsx
+++ src/components/SiteOverview.jsx
@@ -15,9 +15,9 @@ export default class SiteOverview extends Component {
             <Button bsStyle="primary">Learn more</Button>
           </p>
         </Jumbotron>
-        <PageHeader>The 4 main Movement Patterns</PageHeader>
+        <PageHeader className="text-center">The 6 main Movement Patterns</PageHeader>
 
-          <p className='col-xs-8'>
+          <p className='col-xs-12'>
             Generally, we recommend compound exercises that allow you to use a lot of muscle and lift
             heavy weights for your main lifts, regardless of your level of training experience. To
             help with exercise selection, we’ll categorize the compound exercises further according to
@@ -25,14 +25,43 @@ export default class SiteOverview extends Component {
             horizontal push/pull exercises. If you choose from these four, you’ll have your bases mostly
             covered.
           </p>
-          <ol>
-            <li>Knee Dominant</li>
-            <ul>
-              <li>Squat</li>
-            </ul>
-            <li>Hip Dominant</li>
-            <li>Horizontal Push</li>
-            <li>Horizontal Pull</li>
+          <ol className='col-offset-4'>
+            <li>Knee Dominant
+              <ul>
+                <li>Squat</li>
+                <li>Leg extentions</li>
+              </ul>
+            </li>
+            <li>Hip Dominant
+              <ul>
+                <li>Romanian Deadlift</li>
+                <li>Laying Leg curl</li>
+              </ul>
+            </li>
+            <li>Horizontal Push
+              <ul>
+                <li>Barbell Bench Press</li>
+                <li>Push ups</li>
+              </ul>
+            </li>
+            <li>Horizontal Pull
+              <ul>
+                <li>Seated Row</li>
+                <li>Dumbell Row</li>
+              </ul>
+            </li>
+            <li>Vertical Push
+              <ul>
+                <li>Shoulder Press</li>
+                <li>Lateral Raise</li>
+              </ul>
+            </li>
+            <li>Vertical Pull
+              <ul>
+                <li>Pullups</li>
+                <li>Seated Lat Pull</li>
+              </ul>
+            </li>
           </ol>
 
       </div>
